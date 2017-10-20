07715bf72fe7e147c7f5503352f8b595b6b083e3
Ian Pattison <ipattison@outlook.co.nz>
2017-10-20 14:41:24 +1300

    Working on tags

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -54,12 +54,18 @@ class BlogController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository(Post::class)->findLatest($page);
+      $tags = $em->getRepository(Post::class)->findLatest($page);
 
         // Every template name also has two extensions that specify the format and
         // engine for that template.
         // See https://symfony.com/doc/current/templating.html#template-suffix
        // return $this->render('blog/index.'.$_format.'.twig', ['posts' => $posts]);
-      return $this->render('blog/education.html.twig', ['posts' => $posts]);
+      
+      return $this->render('blog/education.html.twig', ['posts' => $posts, 'tags' => $posts]);
+      
+      
+      
+      
     }
   
         /**
